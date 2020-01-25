0cd3e932cabe7b4ac47ed49c010080af41fe1185
fabianra <fabianra999@gmail.com>
2020-01-25 14:35:28 -0500

    feature/component: card

--- src/app/components/shared/cards/cards.component.ts
+++ src/app/components/shared/cards/cards.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-cards',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cards.component.scss']
 })
 export class CardsComponent implements OnInit {
+
+  @Input() items: any[] = [];
 
   constructor() { }
 
